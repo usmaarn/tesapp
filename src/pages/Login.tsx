51169c71aa73fd10c076e1d8de3e3db934e03edc
Usmaarn <usmaarn@gmail.com>
2022-10-10 05:19:08 +0100

    updated login page added border to inputs and styling to submit button

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -9,7 +9,7 @@ function Login (){
                     <img src={LogoSrc} alt="app logo" />
                 </a>
 
-                <div className="grid grid-cols-2 max-w-[1200px] mx-auto f">
+                <div className="grid grid-cols-2 max-w-[1200px] gap-10 mx-auto f">
                     
                     <div className="">
                         <img src={MockupSrc} alt="MOCKUP IMAGE" />
@@ -19,14 +19,14 @@ function Login (){
                         <h1 className="font-avenia_bold text-primary text-4xl">Welcome</h1>
                         <p className="mb-10">Enter Login Details</p>
 
-                        <form className="flex flex-col gap-2 max-w-[400px]">
-                            <input type="text" placeholder="Email Address"  />
+                        <form className="flex flex-col gap-5 max-w-[400px]">
+                            <input type="text" placeholder="Email Address" className="p-3 rounded ring-1 ring-dark/50" />
                             <div className="rounded ring-1 ring-dark/40 text-sm flex items-center">
-                                <input type="password" placeholder="Password" className="bg-transparent px-3 py-2 flex-grow" />
-                                <button type="button" className="uppercase font-semibold text-secondary px-5">show</button>
+                                <input type="password" placeholder="Password" className="bg-transparent p-3 flex-grow" />
+                                <button type="button" className="uppercase font-semibold text-secondary px-5 tracking-wider">show</button>
                             </div>
-                            <a href="#">Forgot password?</a>
-                            <button type="submit">Log in</button>
+                            <a href="#" className="text-secondary font-semibold uppercase text-sm tracking-wider">Forgot password?</a>
+                            <button type="submit" className="p-3 bg-secondary text-white rounded font-bold">Log in</button>
                         </form>
                     </div>
                 </div>
